4064362375e2b0d6657fe73f67c4785f3349c993
范力 <798347235@qq.com>
2021-11-28 15:02:07 +0800

    fix: bug

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App'
 import store from '@/store'
 import router from '@/router'
-import 'element-plus/lib/theme-chalk/index.css'
+import 'element-plus/dist/index.css'
 
 createApp(App).use(router).use(store).mount('#app')
