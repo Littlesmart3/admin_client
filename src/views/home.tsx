95a16de4f38685f4fbca7998ba9797405c9ed899
范力 <798347235@qq.com>
2021-11-28 15:04:05 +0800

    fix: test

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,7 +1,7 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-    setup() {
-        return () => <div>home</div>
-    }
+  setup() {
+    return () => <div>home1</div>
+  }
 })
