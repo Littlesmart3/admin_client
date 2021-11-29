70878a1d41a9d4a3f566ca796abf62727e620b26
范力 <798347235@qq.com>
2021-11-30 00:19:09 +0800

    feat: login 第一阶段

--- src/views/login/login.tsx
+++ src/views/login/login.tsx
@@ -1,46 +1,68 @@
-import { defineComponent } from 'vue'
-import './login.scss'
+import { defineComponent, reactive } from 'vue';
+import './login.scss';
+
+const PasswordCounter = {
+  name: 'password-counter',
+  props: ['count'],
+  methods: {
+    //
+  },
+  render() {
+    return <h1>Password</h1>;
+  }
+};
+
+const QrCodeCounter = {
+  name: 'qrcode-counter',
+  props: ['count'],
+  methods: {
+    //
+  },
+  render() {
+    return <h1>QrCode</h1>;
+  }
+};
 
 export default defineComponent({
   setup() {
-    const ruleForm: any = {
-      pass: 3,
-      age: 2,
-      checkPass: 999
-    }
+    const state = reactive({
+      login_info: { user: '', password: '' }
+    });
+
     return () => (
       <div class="login">
         <div class="row-end h80">
-          <div class="login-box col-center">
-            <div class>
-              <el-form
-                ref="ruleForm"
-                model={ruleForm}
-                status-icon
-                label-width="120px"
-                class="demo-ruleForm"
-              >
-                <el-form-item label="Password" prop="pass">
-                  <el-input v-model={ruleForm.pass} type="password" autocomplete="off"></el-input>
+          <div class="login-box">
+            <div class="title fs40 col-center pb30 pt20">
+              <span class="row-center ">登录</span>
+            </div>
+            <div class="mt20 mlr50">
+              <el-form ref="ruleForm" model={state.login_info} status-icon class="demo-ruleForm">
+                <el-form-item prop="pass">
+                  <el-input v-model={state.login_info.user} autocomplete="off" placeholder="用户名/手机号"></el-input>
                 </el-form-item>
-                <el-form-item label="Confirm" prop="checkPass">
-                  <el-input
-                    v-model={ruleForm.checkPass}
-                    type="password"
-                    autocomplete="off"
-                  ></el-input>
-                </el-form-item>
-                <el-form-item label="Age" prop="age">
-                  <el-input v-model={ruleForm.age}></el-input>
+                <el-form-item prop="checkPass">
+                  <el-input v-model={state.login_info.password} show-password type="password" autocomplete="off" placeholder="密码"></el-input>
                 </el-form-item>
                 <el-form-item>
-                  <el-button onClick="resetForm('ruleForm')">Reset</el-button>
+                  <div class="row-end">
+                    <a class="forget-password" href="javascript:">
+                      忘记密码
+                    </a>
+                  </div>
+                </el-form-item>
+                <el-form-item>
+                  <el-button class="w100 btn-opacity" type="primary" onClick="resetForm('ruleForm')">
+                    登陆
+                  </el-button>
                 </el-form-item>
               </el-form>
+              {/* <PasswordCounter></PasswordCounter>
+              <QrCodeCounter></QrCodeCounter> */}
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
-})
+});
