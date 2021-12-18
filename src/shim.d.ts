b726e464b225129e2c02d04651cce274f4bef8cf
范力 <798347235@qq.com>
2021-12-18 16:34:51 +0800

    feat: login

--- src/shim.d.ts
+++ src/shim.d.ts
@@ -1,10 +1,14 @@
 /* eslint-disable */
 
 declare module '*.vue' {
-  import { defineComponent } from 'vue'
+  import { defineComponent } from 'vue';
   const component: ReturnType<typeof defineComponent> & {
     // install(app: App): void
-  }
-  export default component
+  };
+  export default component;
 }
 
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,35 +1,160 @@
 <template>
-  <div class="home h100">123</div>
+  <div class="home h100">
+    <div class="container" :class="{ 'sign-up-mode': type }">
+      <div class="forms-container">
+        <div class="signin-signup">
+          <form action="#" class="sign-in-form">
+            <h2 class="title">Sign in</h2>
+            <div class="input-field">
+              <div class="row-center col-center">
+                <el-icon class="row-center col-center"><user /></el-icon>
+              </div>
+              <el-input type="text" placeholder="Username" v-model="sign_in.username" />
+            </div>
+            <div class="input-field">
+              <div class="row-center col-center">
+                <el-icon class="row-center col-center"> <lock /></el-icon>
+              </div>
+              <el-input type="password" placeholder="Password" v-model="sign_in.password" show-password />
+            </div>
+            <el-button class="btn solid fs16" @click="signInConfirm">登 录</el-button>
+            <div v-if="false">
+              <p class="social-text">Or Sign in with social platforms</p>
+              <div class="social-media">
+                <a href="#" class="social-icon">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-twitter"></i>
+                </a>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-google"></i>
+                </a>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </div>
+          </form>
+          <form action="#" class="sign-up-form">
+            <h2 class="title">Sign up</h2>
+            <div class="input-field">
+              <div class="row-center col-center">
+                <el-icon class="row-center col-center"> <user /></el-icon>
+              </div>
+              <el-input type="text" placeholder="Username" v-model="sign_up.username" />
+            </div>
+            <div class="input-field">
+              <div class="row-center col-center">
+                <el-icon class="row-center col-center"> <iphone /></el-icon>
+              </div>
+              <el-input type="email" placeholder="Phone" v-model="sign_up.phone" />
+            </div>
+            <div class="input-field">
+              <div class="row-center col-center">
+                <el-icon class="row-center col-center"> <lock /></el-icon>
+              </div>
+              <input type="password" placeholder="Password" v-model="sign_up.password" show-password />
+            </div>
+            <el-button class="btn solid fs16" @click="signUpConfirm">注 册</el-button>
+            <div v-if="false">
+              <p class="social-text">Or Sign up with social platforms</p>
+              <div class="social-media">
+                <a href="#" class="social-icon">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-twitter"></i>
+                </a>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-google"></i>
+                </a>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div class="panels-container">
+        <div class="panel left-panel">
+          <div class="content">
+            <h3>New here ?</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis, ex ratione. Aliquid!</p>
+            <el-button class="btn transparent" @click="signUpBtn">Sign up</el-button>
+          </div>
+          <img :src="log_svg" class="image" alt="" />
+        </div>
+        <div class="panel right-panel">
+          <div class="content">
+            <h3>One of us ?</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laboriosam ad deleniti.</p>
+            <el-button class="btn transparent" @click="signInBtn">Sign in</el-button>
+          </div>
+          <img :src="register_svg" class="image" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, onBeforeMount, reactive, toRefs } from 'vue';
 import { RECORD } from '@/assets/base_info';
+import Log from '@/assets/img/login/log.svg';
+import Register from '@/assets/img/login/register.svg';
+import { User, Lock, Iphone } from '@element-plus/icons-vue';
+import { useRouter } from 'vue-router';
+import { ElMessage } from 'element-plus';
 
 export default defineComponent({
   name: 'home',
-  components: {},
+  components: { User, Lock, Iphone },
   setup() {
+    const router = useRouter();
+
     // 网站备案信息
     const record = RECORD;
+    const log_svg = Log;
+    const register_svg = Register;
     const state = reactive({
-      monitor_width: true
+      type: false, // 注册和登录类型
+      sign_in: { username: '', password: '' },
+      sign_up: { username: '', phone: '', password: '' }
     });
-    const monitorWidth = () => {
-      window.onresize = () => {
-        state.monitor_width = document.documentElement.clientWidth < 750 ? false : true;
-      };
+
+    // 切换注册按钮
+    const signUpBtn = () => (state.type = true);
+
+    // 切换登录按钮
+    const signInBtn = () => (state.type = false);
+
+    // 确认登录
+    const signInConfirm = () => {
+      // const send_data = { username: state.sign_in.username, password: state.sign_in.password };
+      if (state.sign_in.username == 'admin' && state.sign_in.password == '123456') {
+        router.push('/');
+      } else {
+        ElMessage.error('用户名或密码错误！');
+      }
     };
-    onBeforeMount(() => {
-      monitorWidth();
-    });
-    return { ...toRefs(state), record };
+
+    // 确认注册
+    const signUpConfirm = () => {
+      if (state.sign_up.username != '' && state.sign_up.password == '') {
+        router.push('/');
+      }
+    };
+
+    // 导出方法
+    const methods = { signUpBtn, signInBtn, signInConfirm, signUpConfirm };
+    return { ...toRefs(state), log_svg, register_svg, record, ...methods };
   }
 });
 </script>
 
 <style lang="scss">
-.home {
-  color: #000;
-}
+@import 'style.scss';
 </style>
