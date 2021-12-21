a3502bb4c1c7c94bbb6730386fedc88312994768
范力 <798347235@qq.com>
2021-12-21 10:17:58 +0800

    feat: thrmr 11%

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -86,6 +86,9 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+@import '@/assets/scss/dark-style.scss';
+@import '@/assets/scss/light-style.scss';
+
 .header {
   display: flex;
   box-shadow: 0 1px 4px rgb(0 21 41 / 8%);
